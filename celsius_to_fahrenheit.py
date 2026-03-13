266934c2e35eb6075b6fba20ed893fef56582e92
María Fernanda Gonzalez Lara <mariagonlar@unisabana.edu.co>
2026-03-13 12:31:03 -0500

    temp 1

---
+++ celsius_to_fahrenheit.py
@@ -0,0 +1,6 @@
+def celsius_to_fahrenheit(temp):
+    fahrenheit= (temp * 9/5) + 32
+    return float(fahrenheit)
+print(celsius_to_fahrenheit(0))    
+print(celsius_to_fahrenheit(100))  
+print(celsius_to_fahrenheit(-40))
