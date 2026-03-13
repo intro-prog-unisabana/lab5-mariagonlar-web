a27e1d75d1214f0294762687d035e6ec63605e84
María Fernanda Gonzalez Lara <mariagonlar@unisabana.edu.co>
2026-03-13 09:55:11 -0500

    functions 1

---
+++ functions.py
@@ -0,0 +1,13 @@
+calificaciones = [85, 92, 78]
+promedio= float(sum(calificaciones) / len(calificaciones))
+resultado= promedio
+def promedio_estudiante(calificaciones, promedio):
+    return resultado(float)
+print(resultado)
+    
+
+
+
+
+
+    
