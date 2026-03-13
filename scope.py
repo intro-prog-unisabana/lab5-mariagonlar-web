b99fcc9e5a70026a2433acf3142fd7a6c595bd73
María Fernanda Gonzalez Lara <mariagonlar@unisabana.edu.co>
2026-03-13 10:52:31 -0500

    scope 2

--- scope.py
+++ scope.py
@@ -3,7 +3,7 @@ int_value = None
 str_value = None
 
 def set_globals(some_int, some_str):
-    int_value, str_value
+    global int_value, str_value
     int_value = some_int
     str_value = some_str
 
